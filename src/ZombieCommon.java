53b47cb122b896cdfebb1350bbf1b9be6e5ce8c0
Lamont dutcherl@csp.edu <Lamont@192.168.0.36>
2020-06-13 11:51:04 -0500

    Created and Initialized ZombieCommon and ZombieTank classes

---
+++ src/ZombieCommon.java
@@ -0,0 +1,24 @@
+
+public class ZombieCommon {
+
+	public ZombieCommon() {
+		
+	}
+
+	static final Double maxHealth = 30.0;
+	static final Double attack = 5.0;
+
+	private boolean isAlive = true;
+	private Double currentHealth = maxHealth;
+
+	public Double getHealth() {
+
+		return currentHealth;
+
+	}
+
+	public void attack() {
+
+	}
+
+}
---
+++ src/ZombieTank.java
@@ -0,0 +1,24 @@
+
+public class ZombieTank {
+
+	public ZombieTank() {
+		
+	}
+	
+	static final Double maxHealth = 150.0;
+	static final Double attack = 20.0;
+
+	private boolean isAlive = true;
+	private Double currentHealth = maxHealth;
+
+	public Double getHealth() {
+
+		return currentHealth;
+
+	}
+
+	public void attack() {
+
+	}
+
+}

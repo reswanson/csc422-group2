8d2bd5f8db3e53a6d7744f19e0688c8129886929
bschugel1 <66808752+bschugel1@users.noreply.github.com>
2020-06-13 12:30:59 -0500

    Variable Names and GetHealth
    Updated variable name casing so first character lowercase. Added getHealth() method.

--- src/ICharacter.java
+++ src/ICharacter.java
@@ -1,12 +1,13 @@
 
 public interface ICharacter {
 	
-	double MaxHealth=0;			//Maximum Health Value
-	double CurrentHealth=0;		//Current Health Value
-	double Attack=0;			//Attack Value
-	boolean IsAlive=true;		//Boolean for alive status
+	double maxHealth=0;			//Maximum Health Value
+	double currentHealth=0;			//Current Health Value
+	double attack=0;			//Attack Value
+	boolean isAlive=true;			//Boolean for alive status
 	void attack();  			/*Perform attack on all living members of opposing faction 
 								don't see any reason for return value*/
+	double getHealth();			//Return current health
 	
 	//Can add more attributes and methods if determined necessary later.
 	

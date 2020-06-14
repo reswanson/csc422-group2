81b32a7913fb0950429515c22364b4abc15133b3
reswanson <swansonr3@csp.edu>
2020-06-14 16:59:00 -0500

    adding logic to display number of each type of player

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,47 +1,74 @@
 import java.util.*;
 
 public class GameController {
-    //does all the fighting between Survivors and zombies
-    public static void fightRound(ArrayList<ICharacter> attackers, ArrayList<ICharacter> defenders) {
-        
-        for(int i = 0; i < attackers.size(); i++) {
-            for(int j = 0; j < defenders.size(); j++) {
-            
-                attackers.get(i).attack(defenders.get(j));
+	//does all the fighting between Survivors and zombies
+	public static void fightRound(ArrayList<ICharacter> attackers, ArrayList<ICharacter> defenders) {
 
-                if(!defenders.get(j).isAlive()) 
-                    defenders.remove(j);
-                
-                if(defenders.isEmpty())
-                    break;
-            }
-        }
-    }
-    
-    public static void main(String[] args) {
-        //Spawns the Survivors and Zombies for the game
-        ArrayList<ICharacter> survivors = Spawner.spawnRandomSurvivors();
-        ArrayList<ICharacter> zombies = Spawner.spawnRandomZombies();
+		for(int i = 0; i < attackers.size(); i++) {
+			for(int j = 0; j < defenders.size(); j++) {
 
-        System.out.println("We have " + survivors.size() + " survivors trying to make it to safety.");
-        System.out.println("But there are " + zombies.size() + " zombies waiting for them.");
+				attackers.get(i).attack(defenders.get(j));
 
-        //keep fighting back and forth til either Survivors or Zombies are dead
-        while(survivors.size() > 0 && zombies.size() > 0) {
-            fightRound(survivors, zombies);
+				if(!defenders.get(j).isAlive()) 
+					defenders.remove(j);
 
-            if(zombies.isEmpty()) 
-                break;
+				if(defenders.isEmpty())
+					break;
+			}
+		}
+	}
 
-            fightRound(zombies, survivors);
+	public static void main(String[] args) {
+		//Spawns the Survivors and Zombies for the game
+		ArrayList<ICharacter> survivors = Spawner.spawnRandomSurvivors();
+		ArrayList<ICharacter> zombies = Spawner.spawnRandomZombies();
 
-            if(survivors.isEmpty()) 
-                break;
-        }
+		int numOfTank=0;
+		int numOfCommon=0;
+		int numOfChild=0;
+		int numOfTeacher=0;
+		int numOfSoldier=0;
 
-        if(survivors.size() > 0)
-            System.out.println("It seems " + survivors.size() + " have made it to safety.");
-        else
-            System.out.println("There are no survivors.");
-    }
+		//gather numbers of each type of survivors
+		for (ICharacter z : survivors) {
+			if(z instanceof Soldier ){
+				numOfSoldier += 1;
+			} else if(z instanceof Teacher ) {
+				numOfTeacher +=1;
+			} else if (z instanceof Child ) {
+				numOfChild +=1;
+			}
+		}
+
+		//gather numbers of each type of zombie
+		for (ICharacter z : zombies) {
+			if (z instanceof ZombieTank) {
+				numOfTank += 1;
+			} else if (z instanceof ZombieCommon) {
+				numOfCommon +=1;
+			}	
+		}
+
+		System.out.println("We have " + survivors.size() + " survivors trying to make it to safety: ( " + numOfChild + " children, " + numOfTeacher + " teachers, " + numOfSoldier + " soldiers )"  );                     
+		System.out.println("But there are " + zombies.size() + " zombies waiting for them. ( " + numOfTank + " tanks, " + numOfCommon + " tanks )" );
+
+
+		//keep fighting back and forth til either Survivors or Zombies are dead
+		while(survivors.size() > 0 && zombies.size() > 0) {
+			fightRound(survivors, zombies);
+
+			if(zombies.isEmpty()) 
+				break;
+
+			fightRound(zombies, survivors);
+
+			if(survivors.isEmpty()) 
+				break;
+		}
+
+		if(survivors.size() > 0)
+			System.out.println("It seems " + survivors.size() + " have made it to safety.");
+		else
+			System.out.println("There are no survivors.");
+	}
 }

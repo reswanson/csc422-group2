fb190b2ddf56e3032fb165e829655571fb87c650
reswanson <swansonr3@csp.edu>
2020-06-14 17:04:34 -0500

    updating output to match requirements

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -55,7 +55,7 @@ public class GameController {
 		}
 
 	System.out.println("We have " + survivors.size() + " survivors trying to make it to safety: ( " + numOfChild + " children, " + numOfTeacher + " teachers, " + numOfSoldier + " soldiers )"  );                     
-	System.out.println("But there are " + zombies.size() + " zombies waiting for them. ( " + numOfTank + " tanks, " + numOfCommon + " tanks )" );
+	System.out.println("But there are " + zombies.size() + " zombies waiting for them. ( " + numOfCommon + " common, " + numOfTank + " tanks )" );
 
 
         //keep fighting back and forth til either Survivors or Zombies are dead

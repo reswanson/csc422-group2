6e7352db9782ff32b9470546d43f062e6c111287
MiniOranges <user.674711@gmail.com>
2020-06-15 15:38:35 -0500

    Cleanup Soldier.java

--- src/main/java/Soldier.java
+++ src/main/java/Soldier.java
@@ -17,7 +17,7 @@ public class Soldier implements ISurvivor {
 
     @Override
     public void attack(ICharacter victim) {
-	victim.setHealth(victim.getHealth() - attack);
+    	victim.setHealth(victim.getHealth() - attack);
     }
 
     @Override
@@ -42,6 +42,6 @@ public class Soldier implements ISurvivor {
 
     @Override
     public String toString() {
-        return "Soldier" + " " +  id;
+        return String.format("Soldier %d", id);
     }
 }

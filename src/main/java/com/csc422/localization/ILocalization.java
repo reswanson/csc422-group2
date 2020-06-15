ec74e4b3aaf708feaf9133058ae7f3b85586c673
MiniOranges <user.674711@gmail.com>
2020-06-15 15:36:56 -0500

    Add Localization interface

---
+++ src/main/java/com/csc422/localization/ILocalization.java
@@ -0,0 +1,12 @@
+package com.csc422.localization;
+
+/**
+ * Localization interface
+ */
+public interface ILocalization {
+	String getUnitKilled();
+	String getSurvivorStatus();
+	String getZombieStatus();
+	String getSurvivorsWon();
+	String getZombiesWon();
+}

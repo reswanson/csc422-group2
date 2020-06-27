c09cd4b576991bc17562a8b963fc94b25b91b045
bschugel1 <66808752+bschugel1@users.noreply.github.com>
2020-06-27 16:31:29 -0500

    Update GameController.java

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -11,19 +11,12 @@ public class GameController {
         for(int i = 0; i < attackers.size(); i++) {
             for(int j = 0; j < defenders.size(); j++) {
             
-            	
             	ICharacter currentAttacker = attackers.get(i);
             	ICharacter currentDefender = defenders.get(j);
             	
             	
                 currentAttacker.attack(currentDefender);
-                if(currentAttacker instanceof ISurvivor) {
-                System.out.println(((ISurvivor) currentAttacker).getWeapon());
-                }
-                else {
-                	
-                	
-                }
+
                 if(!currentDefender.isAlive()) {
                 	System.out.println("\t" + currentAttacker.toString() + " Killed " + currentDefender.toString());
                     defenders.remove(currentDefender);
@@ -51,37 +44,15 @@ public class GameController {
     	
     	initializeAssets();
     	
-
     	//Spawns the weapons cache for the game
-        ArrayList<IWeapon> cache = Spawner.spawnWeaponCache();
-    	
-
-    	/* Sample code I was learning how to access weapons
-    	Weapon w = AssetLoader.weapons.get(3);
-    	System.out.println("this is the 3th weapon");
-    	System.out.println(w.getId());
-    	System.out.println(w.getAccuracy());
-    	System.out.println(w.getDamage());
-    	System.out.println(w.getName());
-    	
-    	Weapon x = AssetLoader.weapons.get(0);
-    	System.out.println("this is the 0th weapon");
-    	System.out.println(x.getId());
-    	System.out.println(x.getAccuracy());
-    	System.out.println(x.getDamage());
-    	System.out.println(x.getName());
-      */
-
-    	
+        WeaponCache cache = Spawner.spawnWeaponCache();
         //Spawns the Survivors and Zombies for the game
         ArrayList<ICharacter> survivors = Spawner.spawnRandomSurvivors();
         ArrayList<ICharacter> zombies = Spawner.spawnRandomZombies();
         
+        
 
-        // get randnumber between 0 and w.getNumOfWeaponsInCache()
-        //for each survivor, pick a random weapon from cache and assign to survivor using new method.
-
-
+        
         int numOfTank=0;
         int numOfCommon=0;
         int numOfChild=0;

4202ddc7b99a2badafd286e29f39a873e11003fc
reswanson <swansonr3@csp.edu>
2020-06-28 00:44:55 -0500

    Commenting out extra console output per story

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -56,14 +56,14 @@ public class GameController {
         	((ISurvivor) survivor).setWeapon(cache.getWeapon());
         }
         
+        /*
         for(ICharacter survivor : survivors) {
         	System.out.println(survivor + " has a " + ((ISurvivor) survivor).getWeapon());
         	System.out.println("Attack Power: " + survivor.getAttack());
         	System.out.println();
         }
+        */
         
-
-    
 
         int numOfTank=0;
         int numOfCommon=0;
